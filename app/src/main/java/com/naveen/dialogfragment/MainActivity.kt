90271f23ccf91993ef8e6440b7a0e360cc1e92d4
thephotogenicbug <navinkumarachari@gmail.com>
2022-11-02 13:55:19 +0530

    design dialog fragment

--- app/src/main/java/com/naveen/dialogfragment/MainActivity.kt
+++ app/src/main/java/com/naveen/dialogfragment/MainActivity.kt
@@ -2,10 +2,26 @@ package com.naveen.dialogfragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var show : Button
+    lateinit var name : TextView
+    lateinit var age : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        show = findViewById(R.id.buttonShow)
+        name = findViewById(R.id.textViewName)
+        age = findViewById(R.id.textViewAge)
+
+        show.setOnClickListener {
+
+        }
+
     }
 }
